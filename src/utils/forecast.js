7d6f4dd2d1047727899e86c716c70618268615ee
Mrudula <mdeokute@gmail.com>
2021-02-22 12:41:25 +0530

    4. forecast api implemented

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,21 +1,17 @@
 const request = require('request')
 
 const forecast = (latitude, longitude, callback) => {
-    // const url = 'http://api.weatherstack.com/current?access_key=e84129bc20b99e3b1452c36379b4e3ff&query=' + latitude + ',' + longitude + '&units=f'
+    const url = 'http://api.weatherstack.com/current?access_key=c77abca625194ea2fc4c0714926adfb3&query=' + latitude + ',' + longitude + '&units=f'
 
-    // request({ url: url, json: true }, (error, response) => {
-    //     if (error) {
-    //         callback('Unable to connect to weather service!', undefined)
-    //     } else if (response.body.error) {
-    //         callback('Unable to find location', undefined)
-    //     } else {
-    //         callback(undefined, response.body.current.weather_descriptions[0] + ". It is currently " + response.body.current.temperature + " degress out.")
-    //     }
-    // })
-    setTimeout(()=>{
-        callback(undefined, "ABC" + ". It is currently " + "20" + " degress out.")
-    }, 2000);
-    
+    request({ url: url, json: true }, (error, { body }) => {
+        if (error) {
+            callback('Unable to connect to weather service!', undefined)
+        } else if (body.error) {
+            callback('Unable to find location', undefined)
+        } else {
+            callback(undefined, body.current.weather_descriptions[0] + ". It is currently " + body.current.temperature + " degress out. It feels like " + body.current.feelslike + " degress out. The humidity is " + body.current.humidity + "%.")
+        }
+    })
 }
 
 module.exports = forecast
